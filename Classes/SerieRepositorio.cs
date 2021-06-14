1cf4ceeea2451e09041bd004592a8b4e0dd960f8
carloscswd <carloscwd@outlook.com>
2021-06-14 10:57:09 -0300

    App da Digital Inovation One - cadastro de séries em .Net

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using DIO.Series.Interfaces;
+
+namespace DIO.Series
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        // Isso aqui seria o banco de dados
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+
+        public void exclui(int id)
+        {
+            listaSerie[id].Excluir();
+            // Implementa o envio de email aqui
+        }
+
+        public void Insere(Serie objeto)
+        {
+            listaSerie.Add(objeto);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
